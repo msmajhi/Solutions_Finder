6f406d5ff03ac8e239bc586eb9af58672cff068a
msmajhi <122543357+msmajhi@users.noreply.github.com>
2024-07-31 04:39:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ Run this as - $ python main.py "{question url}"
 
 import argparse
 from finder_functions import fetch_page_source, extract_details, process_videos
-
+from display import display_video
 def main(url):
     if not "numerade.com" in url:
-        print("The URL is not from this site.")
-        return
+      print("The URL is not from this site.")
+      return
     
     source_code = fetch_page_source(url)
     final_urls = extract_details(source_code)
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process a URL from numerade.com.')
     parser.add_argument('url', type=str, help='The URL to process')
     args = parser.parse_args()
-    
+      
     main(args.url)
